17c01b3a8409191faab902a7c88ad9be11f85cb4
GIL B. Chan <bnbcmindnpass@gmail.com>
2019-03-25 13:09:09 +0900

    add an alias; fix var resolution bug

--- oh-my-zsh/aliases.kali.zsh
+++ oh-my-zsh/aliases.kali.zsh
@@ -1,1 +1,2 @@
 alias vsc=code . --user-data-dir $HOME/.config/vscode
+alias dotz=cd $DOTZ
--- oh-my-zsh/config.sh
+++ oh-my-zsh/config.sh
@@ -1,5 +1,5 @@
 BASE_DIR=$(dirname "$0")
 
-: "${ZSH_CUSTOM:=HOME/.oh-my-zsh/custom}"
+: "${ZSH_CUSTOM:=$HOME/.oh-my-zsh/custom}"
 
 cp $BASE_DIR/aliases.kali.zsh $ZSH_CUSTOM
